628413d4d4926d5960845e24e6a149f20d76a4b0
Maciej Sputowski <maciejsputowski@onet.pl>
2022-05-10 12:18:03 +0200

    Case change

--- Backend/APPoint.App/Models/DTO/GetAppointmentsForDoctorDTO.cs
+++ Backend/APPoint.App/Models/DTO/GetAppointmentsForDoctorDTO.cs
@@ -1,20 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using APPoint.App.Models.DTO;
+﻿using System.Text.Json.Serialization;
+
 
 namespace APPoint.App.Models.DTO
 {
     public class GetAppointmentsDTO
     {
+        [JsonPropertyName("Monday")]
         public IEnumerable<AppointmentDTO> Monday { get; set; } = default!;
+        [JsonPropertyName("Tuesday")]
         public IEnumerable<AppointmentDTO> Tuesday { get; set; } = default!;
+        [JsonPropertyName("Wednesday")]
         public IEnumerable<AppointmentDTO> Wednesday { get; set; } = default!;
-        public IEnumerable<AppointmentDTO> Thursday{ get; set; } = default!;
+        [JsonPropertyName("Thursday")]
+        public IEnumerable<AppointmentDTO> Thursday { get; set; } = default!;
+        [JsonPropertyName("Friday")]
         public IEnumerable<AppointmentDTO> Friday { get; set; } = default!;
+        [JsonPropertyName("Saturday")]
         public IEnumerable<AppointmentDTO> Saturday { get; set; } = default!;
+        [JsonPropertyName("Sunday")]
         public IEnumerable<AppointmentDTO> Sunday { get; set; } = default!;
     }
 }
