2c3179d17fce2e664b0f2630d91d20136d7ac9b6
macieksputowski <maciejsputowski@onet.pl>
2022-05-14 11:45:46 +0200

    Typo fix

--- Backend/APPoint.App/Services/ICryptographyService.cs
+++ Backend/APPoint.App/Services/ICryptographyService.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace APPoint.App.Services
 {
     public interface ICryptographyService
     {
-        public string Hash(string password, string salt);
+        string Hash(string password, string salt);
 
-        public string GenerateSalt();
+        string GenerateSalt();
     }
 }
